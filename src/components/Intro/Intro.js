1f95ce50717141f1e4ed743d98671194e13614d1
Ongki Herlambang <meepomodeon@gmail.com>
2019-07-11 03:45:28 +0700

    refinement

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -14,14 +14,14 @@ const Hero = ({
   return (
     <Container className={styles.container}>
       <div className={cx(styles.root, className)} {...restProps}>
-        <Text display1 component="h1" breakWord style={{marginBottom: '12px'}}>Invoker<span style={{paddingLeft: '12px', opacity: 0.33}}>101</span></Text>
+        <Text display2 component="h1" breakWord style={{marginBottom: '12px'}}>m.id<span style={{ fontWeight: '400', paddingLeft: '8px', opacity: 0.44}}>invoker</span></Text>
         <Text heading3 component="p" className={styles.intro}>
-          Making well-designed digital products possible for everyone.
+          Making useful and beautiful digital products possible for everyone.
         </Text>
-        <Text medium component="p" className={styles.description}>
-          <strong>Invoker101</strong> is a set of highly opiniated tools, components, and guidelines that helps our team at Meridian.id building website better & faster with best development experiences.
+        <Text large component="p" className={styles.description}>
+          <strong>mi.id</strong><span style={{opacity: 0.54, paddingLeft: '0px'}}>invoker</span> is a set of highly opiniated tools, components, and guidelines that helps our team at Meridian.id building website better & faster with best development experiences.
         </Text>
-        <Text medium component="p" className={styles.description}>
+        <Text large component="p" className={styles.description} style={{ marginBottom: '48px' }}>
           If you like <strong className={styles.highlights}>React Hooks</strong>, <strong className={styles.highlights}>SCSS</strong>, <strong className={styles.highlights}>CSS Modules</strong>, and <strong className={styles.highlights}>Component-based Design</strong>. Give it a try! We sure you will like it too.
         </Text>
         <Button primary large>Get Started</Button>
